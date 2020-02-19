3fbfa454ff438fc5fab4df963cad822876924659
Julien Pause <julien.pause@epitech.eu>
2020-02-19 17:59:01 +0400

    Add cell_state and navy header files

---
+++ include/cell_state.h
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_navy_2019
+** File description:
+** Cell states enumeration
+*/
+
+#ifndef CELL_STATE_H_
+    #define CELL_STATE_H_
+
+    typedef enum cell_state {
+        EMPTY = '.',
+        HIT = 'x',
+        MISSED = 'o',
+    } cell_t;
+    typedef cell_t **grid_t;
+
+    grid_t grid_create(unsigned int rows, unsigned int cols);
+    void print_grid(grid_t grid);
+#endif
