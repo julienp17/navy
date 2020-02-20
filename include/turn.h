84c2cb13056f2761128a6b9a7764ece7e024e29a
Julien Pause <julien.pause@epitech.eu>
2020-02-20 18:30:31 +0400

    Add turn enumeration header file

---
+++ include/turn.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_navy_2019
+** File description:
+** Turn enumeration
+*/
+
+#ifndef TURN_H_
+    #define TURN_H_
+
+    typedef enum turn {
+        PLAYER,
+        ENEMY
+    } turn_t;
+#endif
