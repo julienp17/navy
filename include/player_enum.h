a748933cf4c19418e56b7c9df8c372cf4e5c128d
Julien Pause <julien.pause@epitech.eu>
2020-02-20 19:28:44 +0400

    Replace turn enumeration by player enumeration

---
+++ include/player_enum.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_navy_2019
+** File description:
+** Player enumeration
+*/
+
+#ifndef PLAYER_ENUM_H_
+    #define PLAYER_ENUM_H_
+
+    typedef enum player {
+        FIRST,
+        SECOND
+    } player_t;
+#endif
