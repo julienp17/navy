a9d3a9e8985e585c86a8890ad18b85e560a0c30e
Julien Pause <julien.pause@epitech.eu>
2020-02-20 19:30:24 +0400

    Add previously added functions prototypes in navy header file

--- include/navy.h
+++ include/navy.h
@@ -9,12 +9,17 @@
     #define NAVY_H_
 
     #include <sys/types.h>
-    #include "turn.h"
+    #include "player_enum.h"
     #include "grid.h"
 
     #define NB_BOATS        4
 
     int navy(pid_t enemy_pid, char const *filename);
     int navy_loop(grid_t player_grid, grid_t enemy_grid,
-                pid_t enemy_pid, turn_t turn);
+                pid_t enemy_pid, player_t player);
+
+    void print_grids(grid_t player_grid, grid_t enemy_grid);
+    char *get_input(void);
+    void player_turn(grid_t enemy_grid, pid_t enemy_pid);
+    void enemy_turn(grid_t player_grid, pid_t enemy_pid);
 #endif
