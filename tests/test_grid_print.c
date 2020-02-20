91a86f7d6f837063ebc5fa79c5149aa88349a13a
Julien Pause <julien.pause@epitech.eu>
2020-02-20 13:25:53 +0400

    Refactor grid_print unit tests

--- tests/test_grid_print.c
+++ tests/test_grid_print.c
@@ -12,7 +12,7 @@
 
 Test(print_grid, empty_grid, .init = cr_redirect_stdout)
 {
-    grid_t grid = grid_create(8, 8);
+    grid_t grid = grid_create(8);
     char expected[] =
 " |A B C D E F G H\n"
 "-+---------------\n"
@@ -25,6 +25,29 @@ Test(print_grid, empty_grid, .init = cr_redirect_stdout)
 "7|. . . . . . . .\n"
 "8|. . . . . . . .\n";
 
+    grid_print(grid);
+    cr_assert_stdout_eq_str(expected);
+}
+
+Test(print_grid, pdf_example_1, .init = cr_redirect_stdout)
+{
+    grid_t grid = grid_create(8);
+    char expected[] =
+" |A B C D E F G H\n"
+"-+---------------\n"
+"1|. . 2 . . . . .\n"
+"2|. . 2 . . . . .\n"
+"3|. . . . . . . .\n"
+"4|. . . 3 3 3 . .\n"
+"5|. 4 . . . . . .\n"
+"6|. 4 . . . . . .\n"
+"7|. 4 . 5 5 5 5 5\n"
+"8|. 4 . . . . . .\n";
+
+    grid_place_boat(grid, "2:C1:C2");
+    grid_place_boat(grid, "3:D4:F4");
+    grid_place_boat(grid, "4:B5:B8");
+    grid_place_boat(grid, "5:D7:H7");
     grid_print(grid);
     cr_assert_stdout_eq_str(expected);
 }
