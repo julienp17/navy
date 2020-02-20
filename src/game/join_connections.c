2057ae416c0a3c35dddc259f3f0edd24ffed230a
Julien Pause <julien.pause@epitech.eu>
2020-02-20 18:39:51 +0400

    Retrive second player pid with global variable in join_connections

--- src/game/join_connections.c
+++ src/game/join_connections.c
@@ -11,26 +11,27 @@
 #include <signal.h>
 #include <stdio.h>
 #include "my.h"
+#include "navy.h"
 
-static int wait_connection(void);
-static int connect(pid_t first_player_pid);
+extern int packet;
+
+static pid_t wait_connection(void);
+static pid_t connect(pid_t first_player_pid);
 static void confirm_connection(int sig, siginfo_t *info, void *ucontext);
 
-int join_connections(pid_t first_player_pid)
+int join_connections(pid_t enemy_pid)
 {
-    int exit_code = 0;
-
     my_putstr("my_pid:  ");
     my_putstr(my_int_to_strnum(getpid()));
     my_putchar('\n');
-    if (first_player_pid == 0)
-        exit_code = wait_connection();
+    if (enemy_pid == 0)
+        enemy_pid = wait_connection();
     else
-        exit_code = connect(first_player_pid);
-    return (exit_code);
+        enemy_pid = connect(enemy_pid);
+    return (enemy_pid);
 }
 
-static int wait_connection(void)
+static pid_t wait_connection(void)
 {
     struct sigaction handle_connection;
 
@@ -40,22 +41,22 @@ static int wait_connection(void)
     my_putstr("waiting for enemy connection...\n\n");
     pause();
     my_putstr("enemy connected\n\n");
-    return (0);
+    return (packet);
 }
 
-static int connect(pid_t first_player_pid)
+static pid_t connect(pid_t const enemy_pid)
 {
     struct sigaction handle_connection;
 
     handle_connection.sa_sigaction = confirm_connection;
     sigaction(SIGUSR1, &handle_connection, NULL);
-    if (kill(first_player_pid, SIGUSR2) == -1) {
+    if (kill(enemy_pid, SIGUSR2) == -1) {
         my_puterr("error sending signal to first player process\n");
         return (-1);
     }
     pause();
     my_putstr("successfully connected\n\n");
-    return (0);
+    return (enemy_pid);
 }
 
 static void confirm_connection(int sig, siginfo_t *info, void *ucontext)
@@ -63,6 +64,7 @@ static void confirm_connection(int sig, siginfo_t *info, void *ucontext)
     (void)ucontext;
     if (sig == SIGUSR1)
         return;
+    packet = info->si_pid;
     if (kill(info->si_pid, SIGUSR1) == -1)
         my_puterr("error sending signal to second player process");
 }
