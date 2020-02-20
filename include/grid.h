b6952a226faa91a142983b3b7db89533de180e78
Julien Pause <julien.pause@epitech.eu>
2020-02-20 09:29:25 +0400

    Change grid_create parameters from rows, cols to grid_size

--- include/grid.h
+++ include/grid.h
@@ -8,12 +8,16 @@
 #ifndef GRID_H_
     #define GRID_H_
 
+    #include <stdbool.h>
     #include "cell_state.h"
 
     #define GRID_SIZE           8
 
     typedef cell_t **grid_t;
 
-    grid_t grid_create(unsigned int nb_rows, unsigned int nb_cols);
+    grid_t grid_create(unsigned int grid_size);
     void grid_print(grid_t grid);
+    int grid_place_boat(grid_t grid, char const *format);
+
+    bool pos_is_valid(char const *pos);
 #endif
--- src/structures/grid/grid_create.c
+++ src/structures/grid/grid_create.c
@@ -9,20 +9,20 @@
 #include "grid.h"
 #include "my.h"
 
-grid_t grid_create(unsigned int nb_rows, unsigned int nb_cols)
+grid_t grid_create(unsigned int grid_size)
 {
     grid_t grid = NULL;
 
-    grid = malloc(sizeof(cell_t *) * (nb_rows + 1));
+    grid = malloc(sizeof(cell_t *) * (grid_size + 1));
     if (grid == NULL) {
         my_puterr("Couldn't allocate memory for grid enum\n");
         return (NULL);
     }
-    for (unsigned int row = 0 ; row < nb_rows ; row++) {
-        grid[row] = malloc(sizeof(cell_t) * nb_cols);
-        for (unsigned int col = 0 ; col < nb_cols ; col++)
+    for (unsigned int row = 0 ; row < grid_size ; row++) {
+        grid[row] = malloc(sizeof(cell_t) * grid_size);
+        for (unsigned int col = 0 ; col < grid_size ; col++)
             grid[row][col] = EMPTY;
     }
-    grid[nb_rows] = NULL;
+    grid[grid_size] = NULL;
     return (grid);
 }
