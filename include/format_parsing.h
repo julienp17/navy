f9109e02657034b258dbbace8aeccaf6b7d9e293
Julien Pause <julien.pause@epitech.eu>
2020-02-20 10:08:14 +0400

    Rename file_parsing.h to format_parsing.h

--- include/format_parsing.h
+++ include/format_parsing.h
@@ -2,19 +2,17 @@
 ** EPITECH PROJECT, 2020
 ** PSU_navy_2019
 ** File description:
-** file_parsing
+** Header file for format of a boat line
 */
 
-#ifndef FILE_PARSING_H_
-    #define FILE_PARSING_H_
+#ifndef FORMAT_PARSING_H_
+    #define FORMAT_PARSING_H_
 
     #include <stdbool.h>
 
-    #define NB_BOATS            4
     #define LINE_LEN            7
     #define SEP                 ':'
     #define NB_SEPS             2
-    #define FIRST_BOAT_LEN      2
 
-    bool line_is_valid(char *line, int line_nb);
+    bool format_is_valid(char const *format);
 #endif
