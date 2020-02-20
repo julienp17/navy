d229e6e2e3f609fb8ee28ce88816ae6806c0feac
Julien Pause <julien.pause@epitech.eu>
2020-02-20 13:15:50 +0400

    Add navy header file

--- include/navy.h
+++ include/navy.h
@@ -8,6 +8,10 @@
 #ifndef NAVY_H_
     #define NAVY_H_
 
+    #include <sys/types.h>
+
     #define NB_BOATS        4
 
+    int navy(pid_t first_player_pid, char const *filename);
+    int join_connections(pid_t first_player_pid);
 #endif
