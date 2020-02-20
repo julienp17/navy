3ab0b0df46b06f469441c74fc391fe0d77ce2c80
Julien Pause <julien.pause@epitech.eu>
2020-02-20 18:30:20 +0400

    Add cell_is_boat function prototype in cell_state header file

--- include/cell_state.h
+++ include/cell_state.h
@@ -8,9 +8,13 @@
 #ifndef CELL_STATE_H_
     #define CELL_STATE_H_
 
+    #include <stdbool.h>
+
     typedef enum cell_state {
         EMPTY = '.',
         HIT = 'x',
-        MISSED = 'o',
+        MISS = 'o',
     } cell_t;
+
+    bool cell_is_boat(cell_t cell);
 #endif
