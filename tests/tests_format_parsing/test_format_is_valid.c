11735c0b296e142042d2eb3ccac4db0d7370eafe
Julien Pause <julien.pause@epitech.eu>
2020-02-20 10:11:33 +0400

    Rename tests/tests_file_parsing/ to tests/tests_format_parsing/

--- tests/tests_format_parsing/test_format_is_valid.c
+++ tests/tests_format_parsing/test_format_is_valid.c
@@ -5,58 +5,59 @@
 ** Unit tests to check if a file is valid for navy positions
 */
 
+#include <stdio.h>
 #include <stdbool.h>
 #include <criterion/criterion.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include "file_parsing.h"
+#include "format_parsing.h"
 #include "get_next_line.h"
 
-Test(line_is_valid, incorrect_first_lines)
+Test(format_is_valid, incorrect_first_lines)
 {
     int fd = 0;
     char *line = NULL;
     bool is_valid = false;
 
-    fd = open("incorrect_file.txt", O_RDONLY);
+    fd = open("tests/tests_format_parsing/incorrect_file.txt", O_RDONLY);
     if (fd < 0)
         close(fd);
     line = get_next_line(fd);
     while (line) {
-        is_valid = line_is_valid(line, 0);
+        is_valid = format_is_valid(line);
         cr_assert_eq(is_valid, false);
         line = get_next_line(fd);
     }
 }
 
-Test(line_is_valid, correct_file1)
+Test(format_is_valid, correct_file1)
 {
     int fd = 0;
     char *line = NULL;
     bool is_valid = false;
 
-    fd = open("tests/tests_file_parsing/correct_file1.txt", O_RDONLY);
+    fd = open("tests/tests_format_parsing/correct_file1.txt", O_RDONLY);
     if (fd < 0)
         close(fd);
     for (int i = 0 ; i < 4 ; i++) {
         line = get_next_line(fd);
-        is_valid = line_is_valid(line, i);
+        is_valid = format_is_valid(line);
         cr_assert_eq(is_valid, true);
     }
 }
 
-Test(line_is_valid, correct_file2)
+Test(format_is_valid, correct_file2)
 {
     int fd = 0;
     char *line = NULL;
     bool is_valid = false;
 
-    fd = open("tests/tests_file_parsing/correct_file2.txt", O_RDONLY);
+    fd = open("tests/tests_format_parsing/correct_file2.txt", O_RDONLY);
     if (fd < 0)
         close(fd);
     for (int i = 0 ; i < 4 ; i++) {
         line = get_next_line(fd);
-        is_valid = line_is_valid(line, i);
+        is_valid = format_is_valid(line);
         cr_assert_eq(is_valid, true);
     }
 }
