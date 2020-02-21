e77f2a771a6cb063fd99b54e5743f0177e5e4a39
Julien Pause <julien.pause@epitech.eu>
2020-02-21 15:42:26 +0400

    Change navy_loop return type from int to game_status_t

--- include/navy.h
+++ include/navy.h
@@ -10,16 +10,15 @@
 
     #include <sys/types.h>
     #include "player_enum.h"
+    #include "game_status_enum.h"
     #include "grid.h"
 
     #define NB_BOATS        4
 
     int navy(pid_t enemy_pid, char const *filename);
-    int navy_loop(grid_t player_grid, grid_t enemy_grid,
-                pid_t enemy_pid, player_t player);
+    game_status_t navy_loop(grid_t player_grid, grid_t enemy_grid,
+                            pid_t enemy_pid, player_t player);
 
     void print_grids(grid_t player_grid, grid_t enemy_grid);
     char *get_input(void);
-    void player_turn(grid_t enemy_grid, pid_t enemy_pid);
-    void enemy_turn(grid_t player_grid, pid_t enemy_pid);
 #endif
