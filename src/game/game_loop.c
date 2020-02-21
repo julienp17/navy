efaa6ced04717fd0888502f4939a8025d78bbb92
Julien Pause <julien.pause@epitech.eu>
2020-02-21 16:53:26 +0400

    Refactor game_loop

--- src/game/game_loop.c
+++ src/game/game_loop.c
@@ -22,17 +22,14 @@ static game_status_t enemy_turn(grid_t player_grid, pid_t enemy_pid);
 game_status_t navy_loop(grid_t player_grid, grid_t enemy_grid,
                 pid_t enemy_pid, player_t player)
 {
-    packet = 0;
     if (player == FIRST) {
         if (player_turn(enemy_grid, enemy_pid) != ONGOING)
             return (WON);
-        packet = 0;
         if (enemy_turn(player_grid, enemy_pid) != ONGOING)
             return (LOST);
     } else {
         if (enemy_turn(player_grid, enemy_pid) != ONGOING)
             return (LOST);
-        packet = 0;
         if (player_turn(enemy_grid, enemy_pid) != ONGOING)
             return (WON);
     }
@@ -51,11 +48,10 @@ static game_status_t player_turn(grid_t enemy_grid, pid_t enemy_pid)
     row = coord[1] - '0' - 1;
     send_coord(coord, enemy_pid);
     wait_transmission(1);
-    enemy_grid[row][col] = (packet) ? HIT : MISS;
     my_putstr(coord);
-    my_putstr((enemy_grid[row][col] == HIT) ? ": hit\n\n" : ": missed\n\n");
+    my_putstr((packet == 1) ? ": hit\n\n" : ": missed\n\n");
     free(coord);
-    packet = 0;
+    enemy_grid[row][col] = (packet || enemy_grid[row][col] == HIT) ? HIT : MISS;
     wait_transmission(1);
     return ((packet == 1) ? WON : ONGOING);
 }
@@ -64,20 +60,20 @@ static game_status_t enemy_turn(grid_t player_grid, pid_t enemy_pid)
 {
     int row = 0;
     int col = 0;
+    bool is_hit = false;
     bool is_finished = false;
 
     my_putstr("waiting for enemy's attack...\n");
     wait_transmission(6);
-    col = (packet & 0b00111000) >> 3;
-    row = packet & 0b00000111;
-    player_grid[row][col] = (cell_is_boat(player_grid[row][col])) ? HIT : MISS;
-    usleep(100000);
-    kill(enemy_pid, (player_grid[row][col] == HIT) ? SIGUSR2 : SIGUSR1);
+    col = (packet & COL_BITMASK) >> 3;
+    row = (packet & ROW_BITMASK);
+    is_hit = cell_is_boat(player_grid[row][col]);
+    send_signal(enemy_pid, (is_hit) ? SIGUSR2 : SIGUSR1);
     my_putchar(col + 'A');
     my_putchar(row + '0' + 1);
-    my_putstr((player_grid[row][col] == HIT) ? ": hit\n\n" : ": missed\n\n");
+    my_putstr((is_hit) ? ": hit\n\n" : ": missed\n\n");
+    player_grid[row][col] = (is_hit || player_grid[row][col] == HIT)?HIT : MISS;
     is_finished = !grid_has_boats(player_grid);
-    usleep(100000);
-    kill(enemy_pid, (is_finished) ? SIGUSR2 : SIGUSR1);
+    send_signal(enemy_pid, (is_finished) ? SIGUSR2 : SIGUSR1);
     return ((is_finished) ? LOST : ONGOING);
 }
