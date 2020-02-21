3df5f90b5d37619c0efa119adad5ecc293745093
Julien Pause <julien.pause@epitech.eu>
2020-02-21 16:53:15 +0400

    Reset packet to 0 before waiting for transmission

--- src/signals/wait_transmission.c
+++ src/signals/wait_transmission.c
@@ -2,13 +2,16 @@
 ** EPITECH PROJECT, 2020
 ** PSU_navy_2019
 ** File description:
-** wait_transmission
+** Reset the packet variable and wait for given number of signals
 */
 
 #include <unistd.h>
 
+extern int packet;
+
 void wait_transmission(unsigned int nb_bits)
 {
+    packet = 0;
     for (unsigned int i = 0 ; i < nb_bits ; i++)
         pause();
 }
