bd1381c3354f783f2711b4306681fc5b24c16b63
Julien Pause <julien.pause@epitech.eu>
2020-02-21 16:30:22 +0400

    Add more unit tests for grid create from file

--- tests/tests_grid/test_grid_create_from_file.c
+++ tests/tests_grid/test_grid_create_from_file.c
@@ -42,3 +42,21 @@ Test(grid_create_from_file, correct_file3)
         for (unsigned int j = 0 ; j < GRID_SIZE ; j++)
             cr_assert_eq((cell_is_boat(grid[i][j]) || grid[i][j] == EMPTY), 1);
 }
+
+Test(grid_create_from_file, bad_fd, .init = cr_redirect_stderr)
+{
+    grid_t grid = NULL;
+
+    grid = grid_create_from_file("wouloulou dont exist lol this shoudn't work");
+    cr_assert_null(grid);
+    cr_assert_stderr_eq_str("Couldn't open given file\n");
+}
+
+Test(grid_create_from_file, bad_format_in_file, .init = cr_redirect_stderr)
+{
+    grid_t grid = NULL;
+
+    grid = grid_create_from_file("maps/incorrect_navy.txt");
+    cr_assert_null(grid);
+    cr_assert_stderr_eq_str("Unexpected line found in file area\n");
+}
