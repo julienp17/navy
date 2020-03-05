d162484d2d738ea032d489e11be89fd2659858df
Julien Pause <julien.pause@epitech.eu>
2020-03-05 09:39:45 +0400

    Replace first_player_pid by server_address in main

--- bonus/src/main.c
+++ bonus/src/main.c
@@ -14,13 +14,13 @@
 int main(int ac, char **av)
 {
     char *filename = NULL;
-    pid_t first_player_pid = 0;
+    char *server_address = NULL;
     int exit_code = 0;
 
     if (check_args(ac, av) == -1)
         return (MY_EXIT_FAILURE);
     filename = (ac == 2) ? av[1] : av[2];
-    first_player_pid = (ac == 2) ? 0 : my_strnum_to_int(av[1]);
-    exit_code = navy(first_player_pid, filename);
+    server_address = (ac == 2) ? NULL : av[1];
+    exit_code = navy(server_address, filename);
     return (exit_code);
 }
