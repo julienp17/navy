af3a64ea3c87e11a133d441661e590f1fa166603
Julien Pause <julien.pause@epitech.eu>
2020-03-05 17:59:22 +0400

    Refactor network.h

--- bonus/include/network.h
+++ bonus/include/network.h
@@ -2,11 +2,11 @@
 ** EPITECH PROJECT, 2020
 ** PSU_navy_2019
 ** File description:
-** transmission
+** Handle the network between the first (server) and second player (client)
 */
 
-#ifndef TRANSMISSION_H_
-    #define TRANSMISSION_H_
+#ifndef NETWORK_H_
+    #define NETWORK_H_
 
     #include <sys/types.h>
 
@@ -15,6 +15,7 @@
     #define PORTNUM             2300
 
     typedef int socket_t;
+    typedef unsigned int port_t;
     typedef struct network {
         socket_t server_socket;
         socket_t client_socket;
@@ -22,10 +23,11 @@
 
     char *get_ip_address(void);
 
-    network_t *network_create(char const *server_address, unsigned int const port_number);
+    network_t *network_create(char const *server_address, port_t const port_number);
 
     socket_t server_create(int const port_number);
     socket_t server_accept_connection(socket_t server_socket);
     socket_t client_create(void);
-    int client_connect(socket_t client_socket, char const *server_address, int const port_number);
+    int client_connect(socket_t client_socket, char const *server_address,
+                        int const port_number);
 #endif
