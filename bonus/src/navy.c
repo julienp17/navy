d5cabffb5bcdd56301acf926093d2f3cda1061d6
Julien Pause <julien.pause@epitech.eu>
2020-03-05 17:54:58 +0400

    Refactor navy.c to work with game_structure

--- bonus/src/navy.c
+++ bonus/src/navy.c
@@ -5,38 +5,32 @@
 ** Navy game
 */
 
-#include <stdbool.h>
 #include <stddef.h>
-#include <sys/types.h>
-#include "usage.h"
-#include "grid.h"
-#include "navy.h"
-#include "transmission.h"
-#include "player_enum.h"
-#include "game_status_enum.h"
+#include "game.h"
 #include "my.h"
+#include "navy.h"
 
-transmission_t transmission;
-
-int navy(pid_t enemy_pid, char const *filename)
+int navy(char const *server_address, char const *filename)
 {
-    grid_t player_grid = NULL;
-    grid_t enemy_grid = NULL;
-    player_t player_id = FIRST;
-    game_status_t game_status = ONGOING;
+    int exit_code = 0;
+    game_t *game = NULL;
 
-    player_id = (enemy_pid == 0) ? FIRST : SECOND;
-    player_grid = grid_create_from_file(filename);
-    enemy_grid = grid_create(GRID_SIZE);
-    if (player_grid == NULL || enemy_grid == NULL)
+    game = game_create(filename, server_address);
+    if (game == NULL)
         return (-1);
-    transmission.enemy_pid = join_connections(enemy_pid);
-    if (transmission.enemy_pid == -1)
-        return (MY_EXIT_FAILURE);
-    set_signals_handler();
-    print_grids(player_grid, enemy_grid);
-    while (game_status == ONGOING)
-        game_status = navy_loop(player_grid, enemy_grid, player_id);
-    my_putstr((game_status == WON) ? "I won\n" : "Enemy won\n");
-    return (game_status);
+    exit_code = launch_navy(game);
+    game_destroy(game);
+    return (exit_code);
+}
+
+int launch_navy(game_t *game)
+{
+    print_grids(game->player_grid, game->enemy_grid);
+    while (game->status == ONGOING)
+        game->status = navy_loop(game);
+    my_putstr((game->status == WON) ? "I won\n" :
+            (game->status == LOST) ? "Enemy won\n" :
+            (game->status == NETWORK_ERROR) ? "Connection lost\n":
+            "Unknown error occured\n");
+    return (game->status);
 }
